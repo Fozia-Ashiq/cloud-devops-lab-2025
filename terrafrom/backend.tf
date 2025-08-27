ba0ce02402d29d3345015bcd8d275cefcea0be1e
Fozia-Ashiq <165878620+Fozia-Ashiq@users.noreply.github.com>
2025-08-27 11:46:18 +0500

    Delete terrafrom directory

--- terrafrom/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         =  "terraform-state-360783619044"
-    key            = "infra/terraform.tfstate"
-    region         =  "us-east-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
-}
